3a9b367fee5593a7ea26a51c6adc70b30c90663f
Rachel Killackey <rachel@launchpadlab.com>
2017-05-24 16:10:28 -0500

    start documenting

--- lib/lp_token_auth/core.rb
+++ lib/lp_token_auth/core.rb
@@ -3,6 +3,11 @@ require 'lp_token_auth/error'
 
 module LpTokenAuth
   class << self
+    
+    # Encodes the JWT token
+    # @param [Integer, String] id the id of the resource
+    # @param [Symbol=>String] payload keyword arguments required to create the token
+    # @return [String] token
     def issue_token(id, **payload)
 
       check_id!(id)
@@ -20,6 +25,10 @@ module LpTokenAuth
       )
     end
 
+    # Decodes the JWT token
+    # @param [String] token the token to decode
+    # @raise [LpTokenAuth::Error] if the token is expired, or if any errors occur during decoding
+    # @return [String] token
     def decode!(token)
       begin
         JWT.decode(
@@ -35,6 +44,9 @@ module LpTokenAuth
       end
     end
 
+    # Determines if the `id` provided is either a `String` or an `Integer`
+    # @param [Integer, String] id the identifier of the resource
+    # @raise [LpTokenAuth::Error] if the `id` is not a `String` or `Integer`
     def check_id!(id)
       unless id.is_a?(String) || id.is_a?(Integer)
         raise LpTokenAuth::Error, "id must be a string or integer, you provided #{id}"
