e09652b1bf7f5c723516b4e07c74021713784997
Dan Gutt <Dan Gutt>
2021-06-21 16:43:42 -0500

    Removed an unnecessary conditional

--- lib/lp_token_auth/core.rb
+++ lib/lp_token_auth/core.rb
@@ -65,11 +65,7 @@ module LpTokenAuth
 
     def private_key
         raise LpTokenAuth::Error, 'You do not have a private key.' if ENV['JWE_PRIVATE_KEY'].blank?
-
-        if ENV['JWE_PRIVATE_KEY'].class == Array
-          key = ENV['JWE_PRIVATE_KEY'].join("\n")
-        end
-
+          
         OpenSSL::PKey::RSA.new(key)
       rescue OpenSSL::PKey::RSAError => msg
         raise LpTokenAuth::Error, 'Your private key is formatted incorrectly.'
